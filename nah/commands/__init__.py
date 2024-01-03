90b2670e67e7be08587c4cb18eeff406b3c0ff46
nahom <nahomdev2021@gmail.com>
2024-01-03 16:08:16 +0300

    save changes

--- nah/commands/__init__.py
+++ nah/commands/__init__.py
@@ -1,5 +1,4 @@
-import click
-import frappe
+import click 
 import subprocess
 
 
--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+	install_requires = f.read().strip().split('\n')
+    
 setup(
     name='nah',
     version='1.0.0',
